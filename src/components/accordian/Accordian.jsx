c2346ffc89198df398ef1f1c4b093ca65c374464
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-02-03 17:14:30 +0600

    update

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -38,17 +38,17 @@ const Accordian = () => {
             className="flex justify-center text-2xl gap-3">
                 <input  onClick={()=>{setCheckMultiple(!checkMultiple);setOpen([])}} type="checkbox" id="check" /> <label htmlFor="check">Open multiple  Accordion</label>
             </div>
-          <div className="max-w-3xl border mx-auto ">
+          <div className="max-w-3xl border mx-auto transition-all duration-1000 ease-in-out">
             {allData.map(data=>(<div className="my-3" key={data.id}>
                <div
                 onClick={()=>handleAccordion(data.id)}
-                className="flex px-3 py-2 justify-between  bg-slate-500 text-white">
+                className="flex px-3 py-2 justify-between  bg-slate-500 text-white transition-all duration-1000 ease-in-out">
                 <h1>{data.title}</h1>
                 {open.includes(data.id)? <span>-</span>: <span>+</span>}
                
                </div>
                {
-                open.includes(data.id)? <div className="px-4 bg-slate-300 ">{data.description}</div>:""
+                open.includes(data.id)? <div className="px-4 bg-slate-300 transition-all duration-1000 ease-in-out">{data.description}</div>:""
                }
               
             </div>))}
